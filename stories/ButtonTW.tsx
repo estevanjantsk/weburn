2811d5b811936590309371d49ee928f3eadbdb3a
Estevan Jantsk <estevan.jantsk@gmail.com>
2022-02-22 00:50:33 +0200

    add simple component

---
+++ stories/ButtonTW.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+interface ButtonProps {
+  /**
+   * Is this the principal call to action on the page?
+   */
+  primary?: boolean;
+  /**
+   * What background color to use
+   */
+  backgroundColor?: string;
+  /**
+   * How large should the button be?
+   */
+  size?: 'small' | 'medium' | 'large';
+  /**
+   * Button contents
+   */
+  label: string;
+  /**
+   * Optional click handler
+   */
+  onClick?: () => void;
+}
+
+/**
+ * Primary UI component for user interaction
+ */
+export const ButtonTW = ({
+  primary = false,
+  size = 'medium',
+  backgroundColor,
+  label,
+  ...props
+}: ButtonProps) => {
+  return (
+    <button type="button" {...props} className="bg-sky-500 hover:bg-sky-700 px-5 py-2 text-sm leading-5 rounded-full font-semibold text-white">
+      {label}
+    </button>
+  );
+};
