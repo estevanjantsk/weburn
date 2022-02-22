c078b1f77dbcbc570eea35dba5da6db951b5edbf
Estevan Jantsk <estevan.jantsk@gmail.com>
2022-02-22 17:40:18 +0200

    add wb color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     './stories/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'wb-black': '#1F1E1E',
+        'wb-zinc': '#272727',
+        'wb-red': '#FF1E25',
+        'wb-grey': '#C4C4C4',
+      }
+    },
   },
   plugins: [],
 }
